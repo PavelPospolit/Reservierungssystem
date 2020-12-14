604a688b749a22c1a5b9a4f4d27f184127a54765
Pavel_Pospolit <pavel.pospolit@telekom.de>
2020-12-14 15:50:58 +0100

    Test 23

--- src/AlleListen.java
+++ src/AlleListen.java
@@ -1,7 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
 
-/*Checks if Hashmaps are empty12312313
+/*Checks if Hashmaps are empty123123
  * if not empty: creates Labels and Textareas
  * Names Labels (e.g.: Mitarbeiter, Räume, Reservierungen)
  * puts Data from HashMap into associating Texarea (e.g:: Employee Names into Textarea with the first line "Name:\n"*/
